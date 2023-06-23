1f1e56cfc2a73d07ae9f7a75d438e449b0034677
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-06-23 19:12:42 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   let form = document.querySelector('form');
+//   form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     handleClick(e.target.new-task-description.value)
+//     form.reset();
+//   })
+ 
+
+// });
+
+// function handleClick(todo){
+  
+//   let p = document.createElement('p')
+//   let btn = document.createElement('button')
+//   p.appendChild(btn)
+//   p.textContent = `${todo}`
+//   btn.addEventListener('click',handleDelete)
+//   document.getElementById('tasks').appendChild(p)
+//   console.log(p);
+// }
+// function handleDelete(e){
+//   e.target.parentNode.remove()
+// }
+// OO solution
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // initialize taskList class
+  const taskList = new TaskList();
+  //grab all the necessary DOM elements
+
+  //form and relevant input fields
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  //ul where new tasks will live on the DOM
+  const taskUl = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+  //attach event listeners
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    // reset form
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
